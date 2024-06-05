b0fa2144bd19ee2161e77c791718efb9a2c118d3
Luigi Me <luigi.menez@semifir.com>
2024-06-05 16:01:02 +0200

    feat: :fire: Ajout du dossier data, et implémentation du map et condition like

--- src/components/contenue/Contenue.jsx
+++ src/components/contenue/Contenue.jsx
@@ -1,20 +1,18 @@
 import Techno from "../techno/Techno";
 import styles from "./Contenue.module.scss";
+import { data } from "../../data/technos";
 
 const Contenue = () => {
+  const technos = data;
+
   return (
     <section className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos différentes formations</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <Techno />
-          <Techno />
-          <Techno />
-          <Techno />
-          <Techno />
-          <Techno />
-          <Techno />
-          <Techno />
+          {technos.map((techno) => (
+            <Techno key={techno._id} {...techno} />
+          ))}
         </div>
       </div>
     </section>
--- src/components/techno/Techno.jsx
+++ src/components/techno/Techno.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import styles from "./Techno.module.scss";
-import techno from "../../assets/images/react.webp";
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
 
-const Techno = () => {
+const Techno = ({ nom, image }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleClick = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className={styles.techno}>
       <div className={styles.imageContainer}>
-        <img src={techno} alt="techno" />
+        <img src={image} alt={`Le logo de la techno, ${nom}`} />
       </div>
       <div
-        className={`${styles.technoTitle} d-flex flex-row justify-content-center align-items-center`}
+        className={`${styles.technoTitle} d-flex flex-row justify-content-around align-items-center`}
       >
-        <h3>React</h3>
+        <h3>{nom}</h3>
+        <i onClick={handleClick}>{liked ? <FaHeart /> : <FaRegHeart />}</i>
       </div>
     </div>
   );
